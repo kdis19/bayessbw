e8784b818d1d22396fa694c909df64be3bfb37ba
Kala <kala@morris>
2024-01-11 16:39:15 -0500

    update weather sim code

--- code/new_colonies_ws.R
+++ code/new_colonies_ws.R
@@ -17,16 +17,15 @@ write.csv(on.weather, 'data/on_real_weather.csv', row.names = FALSE)
 in.weather <- subset(weather, Location == 'IN' & Year == 1997)
 write.csv(in.weather, 'data/in_real_weather.csv', row.names = FALSE)
 
+nb.weather <- subset(weather, Location == 'NB' & Year == 1999)
+write.csv(nb.weather, 'data/nb_real_weather.csv', row.names = FALSE)
+
 # ag.gc <- read.csv('code/output/ag_devcurves.csv')
 # ag.gc <- subset(ag.gc, select = -c(low, high))
 # names(ag.gc)[1] <- c('Temp')
 # ag.gc$dev <- ag.gc$mid/24
 
 gc.df <- read.csv('code/output/all_posterior_curves.csv')
-
-gc.cast <- dcast(gc.df, temp + province + index ~ stage, value.var = 'dev')
-names(gc.cast)[names(gc.cast) == 'temp'] <- 'Temp'
-mg <- merge(on.weather, gc.cast)
 
 dev.traverse <- function(df) {
   #dates <- df$DATE
@@ -53,34 +52,51 @@ dev.traverse <- function(df) {
   return(dev.vec)
 }
 
-mg$full.index <- paste0(mg$province, '_', mg$index)
-dt.lst <- lapply(unique(mg$full.index), function(ind) {
-  sub <- subset(mg, full.index == ind)
-  sub <- sub[order(sub$Hour),]
-  sub <- sub[order(sub$JDay),]
-  dt <- dev.traverse(sub)
-  sub$age <- dt
-  return(sub)
-})
-age.df <- bind_rows(dt.lst)
-write.csv(age.df, 'code/output/sim_age.csv')
-ag.age.df <- aggregate(data = age.df, age ~ DATE + province + index + full.index, max)
-write.csv(ag.age.df, 'code/output/sim_age_daily.csv', row.names = FALSE)
+age.fun <- function(Prov, year) {
+  prov <- tolower(Prov)
+  yr <- substr(as.character(year), 3, 4)
+  
+  wthr <- subset(weather, Location == Prov & Year == year)
+  write.csv(wthr, paste0('data/', prov, yr, '_real_weather.csv'))
+  
+  gc.cast <- dcast(gc.df, temp + province + index ~ stage, value.var = 'dev')
+  names(gc.cast)[names(gc.cast) == 'temp'] <- 'Temp'
+  mg <- merge(wthr, gc.cast)
+  
+  mg$full.index <- paste0(mg$province, '_', mg$index)
+  dt.lst <- lapply(unique(mg$full.index), function(ind) {
+    sub <- subset(mg, full.index == ind)
+    sub <- sub[order(sub$Hour),]
+    sub <- sub[order(sub$JDay),]
+    dt <- dev.traverse(sub)
+    sub$age <- dt
+    return(sub)
+  })
+  age.df <- bind_rows(dt.lst)
+  ag.age.df <- aggregate(data = age.df, age ~ DATE + province + index + full.index, max)
+  write.csv(ag.age.df, paste0('code/output/weathersims/', 
+            Prov, yr, '_sim_age.csv'), row.names = FALSE)
+  
+  p.lst <- lapply(unique(age.df$full.index), function(ind) {
+    sub <- subset(age.df, full.index == ind)
+    w <- min(which(sub$age == 5))
+    pupation <- sub$JDay[w]
+    ind.split <- strsplit(ind, split = '_')[[1]]
+    pup.df <- data.frame('province' = ind.split[1], 
+                         'index' = ind.split[2],
+                         'pup.jd' = pupation)
+    return(pup.df)
+  })
+  p.df <- bind_rows(p.lst)
+  
+  write.csv(p.df, paste0('code/output/weathersims/', 
+                         Prov, yr, '_pupal_dates.csv'), row.names = FALSE)
+  return(age.df)
+}
+
 
 ##### Compare pupal dates #####
-p.lst <- lapply(unique(age.df$full.index), function(ind) {
-  sub <- subset(age.df, full.index == ind)
-  w <- min(which(sub$age == 5))
-  pupation <- sub$JDay[w]
-  ind.split <- strsplit(ind, split = '_')[[1]]
-  pup.df <- data.frame('province' = ind.split[1], 
-                       'index' = ind.split[2],
-                       'pup.jd' = pupation)
-  return(pup.df)
-})
-p.df <- bind_rows(p.lst)
 
-write.csv(p.df, 'code/output/pupal_dates.csv', row.names = FALSE)
 
 yr.lst <- lapply(unique(p.df$index), function(ind) {
   sub <- subset(p.df, index == ind)
